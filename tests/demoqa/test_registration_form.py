23d5df17d1d053d7beb565d726525ff8fa10b35e
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-28 19:12:03 +0300

    refactoring

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -3,44 +3,10 @@ import os
 import allure
 from selene import have, by
 
-from utils import attach
-
 
 @allure.title("Successful fill form")
 def test_successful(setup_browser):
     browser = setup_browser
-    # first_name = "Alex"
-    # last_name = "Egorov"
-    #
-    # with allure.step("Open registrations form"):
-    #     browser.open("https://demoqa.com/automation-practice-form")
-    #     browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
-    #     browser.driver.execute_script("$('footer').remove()")
-    #     browser.driver.execute_script("$('#fixedban').remove()")
-    #
-    # with allure.step("Fill form"):
-    #     browser.element("#firstName").set_value(first_name)
-    #     browser.element("#lastName").set_value(last_name)
-    #     browser.element("#userEmail").set_value("alex@egorov.com")
-    #     browser.element("#genterWrapper").element(by.text("Other")).click()
-    #     browser.element("#userNumber").set_value("1231231230")
-    #     # browser.element("#dateOfBirthInput").click()
-    #     # browser.element(".react-datepicker__month-select").s("July")
-    #     # browser.element(".react-datepicker__year-select").selectOption("2008")
-    #     # browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
-    #     browser.element("#subjectsInput").send_keys("Maths")
-    #     browser.element("#subjectsInput").press_enter()
-    #     browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
-    #     # browser.element("#uploadPicture").uploadFromClasspath("img/1.png")
-    #     browser.element("#currentAddress").set_value("Some street 1")
-    #     browser.element("#state").click()
-    #     browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
-    #     browser.element("#city").click()
-    #     browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
-    #     browser.element("#submit").click()
-    #
-    # with allure.step("Check form results"):
-    #     browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
 
     with allure.step("Open registrations form"):
         browser.open('https://demoqa.com/automation-practice-form')
@@ -68,10 +34,11 @@ def test_successful(setup_browser):
         browser.element('[id="currentAddress"]').send_keys('Test-city, test street, test house 2')
 
         browser.element('#state').click()
-        browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('NCR')).click()
-        browser.element('#react-select-4-input').type('Delhi').press_enter()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element("#submit").click()
 
-        browser.element('#submit').press_enter()
     with allure.step("Check form results"):
         browser.all('tbody tr').should(have.exact_texts(
             'Student Name Ivan Ivanov', 'Student Email r@ya.com', 'Gender Male', 'Mobile 7777777777',
