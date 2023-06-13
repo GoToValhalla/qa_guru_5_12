baee2e8f4942d01d2fb56a37f2d729662a61d9f7
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-06-13 20:58:19 +0300

    add simple tests

---
+++ tests/simple/test_skipped.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.skip
+def test_skipped1():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped2():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped3():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped4():
+    assert False
+
+@pytest.mark.skip
+def test_skipped5():
+    assert False
